a5b77d66e17ebd3d7be61fe8165e90a0deec66e2
Nataliya <gnykka@gmail.com>
2024-03-26 00:42:23 +0400

    Set up tailwind and basic styling

---
+++ src/components/App/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main className="w-full h-full">
+      <h1>Header 1</h1>
+      <h2>Header 2</h2>
+      <h3>Header 3</h3>
+      <p>normal text</p>
+      <p className="text-small">small text</p>
+      <a href="#">simple link</a>
+      <button className="button">simple button</button>
+    </main>
+  );
+}
+
+export default App;
